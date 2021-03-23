ab99f8dcaff298b80098e3f6c50468d986c73ad5
Seunghwan Ian Lee <78548615+lsh41610@users.noreply.github.com>
2021-03-23 15:33:44 +0900

    edit comment

--- bash_ssh_conf_4_CentOS.sh
+++ bash_ssh_conf_4_CentOS.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-#ansible이 설치되지 않은 node용.  bash shell 프로그래밍으로 수정
+#ansible이 설치되지 않은 node용으로,  bash shell 프로그래밍으로 수정
 now=$(date +"%m_%d_%Y")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup #원본파일 백업
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config #스트림편집기로 PasswordAuthentication 수정
